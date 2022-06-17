919240c431d71095d7cb19fa4b561e105b333bbd
赵光南 <zgn@tensun.cn>
2022-06-17 13:51:13 +0800

    feature:添加了每页的界面

--- js/theme.js
+++ js/theme.js
@@ -176,84 +176,15 @@ document.addEventListener("DOMContentLoaded", function () {
     /* ==============================================
         GET COUNTRIES FROM AJAX REQUEST
     ============================================== */
-    const countries = document.getElementById("country");
-    const states = document.getElementById("state");
-
-    let countriesRequest = new XMLHttpRequest();
-    countriesRequest.open("GET", "js/countries.json");
-    countriesRequest.onload = function () {
-        let requestData = JSON.parse(countriesRequest.responseText);
-        renderCountries(requestData);
-    };
-    countriesRequest.send();
-
-    function renderCountries(data) {
-        let htmlString = "";
-        for (let i = 0; i < data.length; i++) {
-            htmlString += `<option value='${data[i].countryShortCode}'>${data[i].countryName}</option>`;
-        }
-
-        if (countries) {
-            countries.insertAdjacentHTML("beforeend", htmlString);
-            /* =====================================================
-                CUSTOM SELECT
-            ===================================================== */
-            const countrieschoices = new Choices(countries, {
-                placeholder: true,
-                searchEnabled: false,
-                itemSelectText: "",
-                callbackOnInit: function () {
-                    let pickerCustomClass = countries.dataset.customclass;
-                    let pickerSevClasses = pickerCustomClass.split(" ");
-                    countries.parentNode.classList.add.apply(countries.parentNode.classList, pickerSevClasses);
-                },
-            });
-        }
-    }
 
     /* =====================================================
         CUSTOM SELECT
     ===================================================== */
-    window.onload = function () {
-        if (states) {
-            const stateschoices = new Choices(states, {
-                placeholder: true,
-                searchEnabled: false,
-                itemSelectText: "",
-                callbackOnInit: function () {
-                    let pickerCustomClass = states.dataset.customclass;
-                    let pickerSevClasses = pickerCustomClass.split(" ");
-                    states.parentNode.classList.add.apply(states.parentNode.classList, pickerSevClasses);
-                },
-            });
-        }
-    };
+
 
     /* =====================================================
         NAVBAR BEHAVIOR
     ===================================================== */
-    function navBarBehavior() {
-        const topBar = document.getElementById("topBar"),
-            topBarHeight = topBar.offsetHeight,
-            header = document.querySelector(".make-sticky"),
-            headerHeight = header.offsetHeight,
-            scroll = window.pageYOffset;
-
-        if (scroll >= topBarHeight) {
-            header.classList.add("is-fixed", "shadow-sm");
-            document.body.style.paddingTop = `${headerHeight}px`;
-        } else {
-            header.classList.remove("is-fixed", "shadow-sm");
-            document.body.style.paddingTop = "0";
-        }
-    }
-
-    window.addEventListener("scroll", function () {
-        navBarBehavior();
-    });
-    window.addEventListener("resize", function () {
-        navBarBehavior();
-    });
 
     /* ==============================================
         MULTILEVEL DROPDOWNS
